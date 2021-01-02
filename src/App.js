ebc43edf66726acc804fbfeedcb4133398aad22f
fernando-meira <thomwork@outlook.com>
2021-01-02 16:48:32 -0300

    feat: 🎸 Challenge concluded

--- src/App.js
+++ src/App.js
@@ -1,58 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+
+import api from "./services/api"
 
 import {
-  SafeAreaView,
   View,
-  FlatList,
   Text,
+  FlatList,
   StatusBar,
   StyleSheet,
+  SafeAreaView,
   TouchableOpacity,
-} from "react-native";
+} from "react-native"
 
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    try {
+      const { data } = await api.post(`repositories/${id}/like`)
+
+      const updatedRepositories = repositories.map((repository) => {
+        if (repository.id === id) {
+          return data
+        } else {
+          return repository
+        }
+      })
+
+      setRepositories(updatedRepositories)
+    } catch (error) {
+      console.log(error)
+    }
   }
+
+  useEffect(() => {
+    async function fetchRepositries() {
+      try {
+        const { data } = await api.get("repositories")
+
+        setRepositories(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchRepositries()
+  }, [])
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        {repositories && (
+          <FlatList
+            data={repositories}
+            keyExtractor={(repository) => repository.id}
+            renderItem={({ item: repository }) => (
+              <View style={styles.repositoryContainer}>
+                <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+                <FlatList
+                  style={styles.techsContainer}
+                  data={repository.techs}
+                  keyExtractor={(tech) => tech}
+                  renderItem={({ item: tech }) => (
+                    <Text style={styles.tech}>{tech}</Text>
+                  )}
+                />
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+                <View style={styles.likesContainer}>
+                  <Text
+                    style={styles.likeText}
+                    testID={`repository-likes-${repository.id}`}
+                  >
+                    {repository.likes <= 1
+                      ? `${repository.likes} curtida`
+                      : `${repository.likes} curtidas`}
+                  </Text>
+                </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(repository.id)}
+                  testID={`like-button-${repository.id}`}
+                >
+                  <Text style={styles.buttonText}>Curtir</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          />
+        )}
       </SafeAreaView>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -104,4 +143,4 @@ const styles = StyleSheet.create({
     backgroundColor: "#7159c1",
     padding: 15,
   },
-});
+})
